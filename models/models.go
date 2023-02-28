cd8214fd9fd216c3797194e43d2943c6ad798ee2
Matei Cheles <mateicheles1@gmail.com>
2023-02-28 10:59:53 +0200

    v2

---
+++ models/models.go
@@ -0,0 +1,17 @@
+package models
+
+type ToDo struct {
+	Id      string `json:"id"`
+	ListId  string `json:"listid"`
+	Content string `json:"content"`
+}
+
+type ToDoList struct {
+	Id    string `json:"listId"`
+	Owner string `json:"owner"`
+	Todos []ToDo `json:"todos"`
+}
+
+type AppData struct {
+	Data []ToDoList `json:"data"`
+}
